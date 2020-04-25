258cef581084e25d877e84938363a2bfa6ac1cbf
Tomasz <tom.g2807@gmail.com>
2020-04-25 13:28:07 +0200

    init commit

---
+++ src/main/java/Gruzdzis/Wicki/ankieter/AnkieterApplication.java
@@ -0,0 +1,24 @@
+package Gruzdzis.Wicki.ankieter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AnkieterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkieterApplication.class, args);
+	}
+
+}
+
+
+@RestController
+class HelloTest {
+	@GetMapping("/test")
+	String test() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/Gruzdzis/Wicki/ankieter/AnkieterApplicationTests.java
@@ -0,0 +1,13 @@
+package Gruzdzis.Wicki.ankieter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkieterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
