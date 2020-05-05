aefa0cba9db181bf4616d2c9cad0cf3b9042230e
Tomasz <tom.g2807@gmail.com>
2020-05-05 14:13:05 +0200

    error test

---
+++ src/main/java/Gruzdzis/Wicki/ankieter/Api/Error_Api.java
@@ -0,0 +1,19 @@
+package Gruzdzis.Wicki.ankieter.Api;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Error_Api implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
